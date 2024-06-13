0618b8721ce5bff6bfd68826e485d97fc7072b6c
Larz <larzuck41@gmail.com>
2024-06-13 15:42:09 -0500

    Mostrar mensaje de error

--- SPA/src/assets/js/informes.js
+++ SPA/src/assets/js/informes.js
@@ -2,6 +2,7 @@ let opcion = document.getElementById("opcionInforme");
 let fechaInicio = document.getElementById("fechaInicio");
 let fechaFin = document.getElementById("fechaFin");
 let generarInforme = document.getElementById("generarInforme");
+let mensaje = document.getElementById("mensaje");
 
 opcion.addEventListener("change", function () {
   if (opcion.value == "Ingresos" || opcion.value == "Ocupacion") {
@@ -30,9 +31,16 @@ generarInforme.addEventListener("click", function () {
 
         .then(function (data) {
           console.log(data);
+          mensaje.innerText = ""
+          mensaje.setAttribute("class","")
           //funcion
         });
+      
     }
+  }
+  else{
+    mensaje.innerText = "Seleccione el límite de búsqueda, por favor"
+    mensaje.setAttribute("class","bg-warning rounded p-2 m-3  text-white")
   }
 
   if (
@@ -48,9 +56,16 @@ generarInforme.addEventListener("click", function () {
 
         .then(function (data) {
           console.log(data);
+          mensaje.innerText = ""
+          mensaje.setAttribute("class","")
           //funcion
         });
     }
+
+  }
+  else{
+    mensaje.innerText = "Seleccione el límite de búsqueda, por favor"
+    mensaje.setAttribute("class","bg-warning rounded p-2 m-3 text-white")
   }
 
   if (opcion.value == "Clientes") {
